7d6a451a64f5f7d7eb1d438b08078aa26fd39e17
Nina <ninarimsky@gmail.com>
2021-03-19 21:01:45 +0000

    initial commit

---
+++ src/App/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-light navbar-expand-lg px-2 px-md-5">
+        <NavLink exact to="/" className="navbar-brand">HappiNetwork</NavLink>
+        <ul className="navbar-nav">
+            <li className="nav-item active"><NavLink exact to="/" className="nav-link">Home</NavLink></li>
+            <li className="nav-item active"><NavLink to="/about" className="nav-link">About</NavLink></li>
+        </ul>
+    </nav>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './bootstrap.css';
+import './index.css';
+import App from './App/App';
+
+// Leave this code as is - it just renders the app
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
