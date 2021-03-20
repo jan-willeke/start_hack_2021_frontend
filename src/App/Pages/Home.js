a48ba6039e24f481bb4b86eb624bb7ecf3d178db
Nina <ninarimsky@gmail.com>
2021-03-20 15:00:37 +0000

    UI design changes

--- src/App/Pages/Home.js
+++ src/App/Pages/Home.js
@@ -5,8 +5,8 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
     return <div className="container p-4">
         <h1>We are HappiNetwork.</h1>
-        <h3>Say goodbye to feeling isolated when working from home. Our tool enables remote teams to network and share knowledge in an effective and most importantly <span className="text-primary">fun</span> way.</h3>
-        <h3>We help nurture collaboration and social relations within a company whether it's the first day of an employee or they are are a "veteran".</h3>
+        <p className="larger">Say goodbye to feeling isolated when working from home. Our tool enables remote teams to network and share knowledge in an effective and most importantly <span className="text-primary">fun</span> way.</p>
+        <p className="larger">We help nurture collaboration and social relations within a company whether it's the first day of an employee or they are are a "veteran".</p>
         <HomeOffice className="background-image" />
         <NavLink to="/questions" className="btn btn-primary mt-4 btn-lg">Start your networking journey</NavLink>
     </div>;
