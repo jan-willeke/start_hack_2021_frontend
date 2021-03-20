17280381a62ac2bb551c358851f395c4e8526f83
Nina <ninarimsky@gmail.com>
2021-03-20 00:22:39 +0000

    remove blah blah

--- src/App/Pages/Home.js
+++ src/App/Pages/Home.js
@@ -3,7 +3,7 @@ import {ReactComponent as HomeOffice} from "../../Img/HomeOffice.svg";
 
 export default function Home() {
     return <div className="container m-4">
-        <h1>We are HappiNetwork Blah Blah.</h1>
+        <h1>We are HappiNetwork.</h1>
         <h3>Say goodbye to feeling isolated when working from home. Our tool enables remote teams to network and share knowledge in an effective and most importantly <span className="text-primary">fun</span> way.</h3>
         <HomeOffice className="background-image"/>
     </div>;
