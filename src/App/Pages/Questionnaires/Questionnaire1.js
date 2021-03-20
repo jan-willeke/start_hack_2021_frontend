1f9711a5a96135771a88f6485d419f543565397a
Nina <ninarimsky@gmail.com>
2021-03-20 15:28:20 +0000

    fix import error

--- src/App/Pages/Questionnaires/Questionnaire1.js
+++ src/App/Pages/Questionnaires/Questionnaire1.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react'
-import RubberSlider from '@shwilliam/react-rubber-slider'
-import '@shwilliam/react-rubber-slider/dist/styles.css'
 
 export default function Questionnaire1() {
 
@@ -8,6 +6,5 @@ export default function Questionnaire1() {
 
     return <div className="container p-4">
         <h1>Expand your horizons</h1>
-        <RubberSlider width={250} value={value} onChange={setValue} />
     </div>;
 }
