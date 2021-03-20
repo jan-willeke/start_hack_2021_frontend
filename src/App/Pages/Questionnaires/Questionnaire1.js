e793aed699d9c7c86b242ced0f2fb544311236fc
Nina <ninarimsky@gmail.com>
2021-03-20 15:34:10 +0000

    fix warnings

--- src/App/Pages/Questionnaires/Questionnaire1.js
+++ src/App/Pages/Questionnaires/Questionnaire1.js
@@ -1,8 +1,6 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 export default function Questionnaire1() {
-
-    const [value, setValue] = useState(0.5);
 
     return <div className="container p-4">
         <h1>Expand your horizons</h1>
