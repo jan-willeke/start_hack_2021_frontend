fdf4bdf860ae93d1e3be82df1af02c679011d5d7
muliPuli <milan.paczai20@imperial.ac.uk>
2021-03-21 03:19:51 +0100

    changes in text

--- src/App/Pages/IdeaHub.js
+++ src/App/Pages/IdeaHub.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 export default function IdeaHub() {
     return <div className="container p-4">
         <h1 className="text-primary">Idea Hub</h1>
-        <p className="larger">Here you can post suggestions about anything directly or indirectly related to your company. Furthermore, you have the option to vote on suggestions proposed by your fellow peers. Democracy nurtures innovation.</p>
+        <p className="larger">Here you can post suggestions about anything directly or indirectly related to your company. You also have the option to vote on suggestions proposed by your fellow peers. <br></br> <span className="text-primary">Democracy nurtures innovation.</span></p>
        
         <HomeOffice className="background-image" />
         <NavLink to="/" className="btn btn-primary mt-4 btn-lg">Post a Suggestion</NavLink>
--- src/App/Pages/Leaderboard.js
+++ src/App/Pages/Leaderboard.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 export default function Leaderboard() {
     return <div className="container p-4">
         <h1 className="text-primary">This week's Team Leaderboard</h1>
-        <p className="larger">Say goodbye to feeling isolated when working from home. Our tool enables remote teams to network and share knowledge in an effective and most importantly <span className="text-primary">fun</span> way.</p>
+        <p className="larger">On this tab, you can see the results of this week's Friday Networking Hour event.</p>
         <HomeOffice className="background-image" />
         
     </div>;
