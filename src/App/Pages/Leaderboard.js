654616e31b4bfa8a832f501a1de157f8ab992c43
muliPuli <milan.paczai20@imperial.ac.uk>
2021-03-21 02:59:14 +0100

    NewTabs function name updates

--- src/App/Pages/Leaderboard.js
+++ src/App/Pages/Leaderboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as HomeOffice } from "../../Img/HomeOffice.svg";
 import { NavLink } from "react-router-dom";
 
-export default function Home() {
+export default function Leaderboard() {
     return <div className="container p-4">
         <h1 className="text-primary">This week's Team Leaderboard</h1>
         <p className="larger">Say goodbye to feeling isolated when working from home. Our tool enables remote teams to network and share knowledge in an effective and most importantly <span className="text-primary">fun</span> way.</p>
--- src/App/Pages/MyStats.js
+++ src/App/Pages/MyStats.js
@@ -2,11 +2,11 @@ import React from "react";
 import { ReactComponent as HomeOffice } from "../../Img/HomeOffice.svg";
 import { NavLink } from "react-router-dom";
 
-export default function Home() {
+export default function MyStats() {
     return <div className="container p-4">
 
         <h1 className="text-primary">My stats</h1>
-        <p className="larger">You can find the number of connections you have, the quality of the time you have spent together,  <span className="text-primary">fun</span> way.</p>
+        <p className="larger">You can find information about your connections, your previous Teams and projects and your Rewards here.<span className="text-primary">fun</span> way.</p>
         <HomeOffice className="background-image" />
         <NavLink to="/me" className="btn btn-primary mt-4 btn-lg">Start your PeerNet networking journey</NavLink>
     </div>;
