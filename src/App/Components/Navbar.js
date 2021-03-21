2098b17bf5646f06586cc717d17cc8b84e10f15a
Nina <ninarimsky@gmail.com>
2021-03-21 07:45:57 +0000

    improve navbar

--- src/App/Components/Navbar.js
+++ src/App/Components/Navbar.js
@@ -4,15 +4,15 @@ import logo from '../../Img/logo.png';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-light bg-light px-2 px-md-5 navbar-expand">
+    <nav className="navbar navbar-light bg-light px-2 px-md-5 navbar-expand-lg">
       <NavLink exact to="/" className="navbar-brand"><img src={logo} alt="Logo" className="logo" /></NavLink>
       <ul className="navbar-nav">
-        <li className="nav-item active"><NavLink exact to="/" className="nav-link">Home</NavLink></li>
-        <li className="nav-item active"><NavLink to="/about" className="nav-link">About</NavLink></li>
-        <li className="nav-item active"><NavLink to="/me" className="nav-link">Start</NavLink></li>
-        <li className="nav-item active"><NavLink to="/leaderboard" className="nav-link">Team Leaderboard</NavLink></li>
-        <li className="nav-item active"><NavLink to="/ideahub" className="nav-link">Idea Hub</NavLink></li>
-        <li className="nav-item active"><NavLink to="/mystats" className="nav-link">My Stats</NavLink></li>
+        <li className="nav-item active"><NavLink exact to="/" className="nav-link">🏠 Home</NavLink></li>
+        <li className="nav-item active"><NavLink to="/about" className="nav-link">🌐 About</NavLink></li>
+        <li className="nav-item active"><NavLink to="/me" className="nav-link">🏁 Start</NavLink></li>
+        <li className="nav-item active"><NavLink to="/leaderboard" className="nav-link">📋 Team Leaderboard</NavLink></li>
+        <li className="nav-item active"><NavLink to="/ideahub" className="nav-link">💡 Idea Hub</NavLink></li>
+        <li className="nav-item active"><NavLink to="/mystats" className="nav-link">📈 My Stats</NavLink></li>
       </ul>
     </nav>
   );
